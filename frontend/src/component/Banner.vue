d82e50a763a262a5c219acdfbb89f10f09a212e5
YSZ <2300012984@stu.pku.edu.cn>
2025-10-10 22:29:42 +0800

    basic mode

--- frontend/src/component/Banner.vue
+++ frontend/src/component/Banner.vue
@@ -1,21 +1,21 @@
 <script setup></script>
 <template>
   <div class="bg-white shadow-md">
-    <div class="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+    <div class="max-w-7xl mx-auto px-10 py-4 flex justify-between items-center">
             <h1 class="text-xl font-bold text-pkured">PKU 智慧助手</h1>
 
-            <nav class="flex space-x-6 justify-between items-center">
-                <a href="/" class="hover:text-pkured">个人资料</a>
-                <a href="/" class="hover:text-pkured">
-                  <img src="../assets/cloud.svg" class="h-8"></img>
-                </a>
-                <a href="/" class="hover:text-pkured">
-                  <img src="../assets/client.svg" class="h-8"></img>
-                </a>
-                <button class="hover:text-pkured" onclick="logout()">
-                <img src="../assets/logout.svg" class="h-8"></img>
+            <nav class="flex items-center gap-4">
+                <button class="p-2 rounded-lg hover:cursor-pointer hover:bg-gray-200">
+                  <img src="../assets/cloud.svg" class="h-5"></img>
+                </button>
+                <button class="p-2 rounded-lg hover:cursor-pointer hover:bg-gray-200">
+                  <img src="../assets/client.svg" class="h-5"></img>
+                </button>
+                <button class="p-2 rounded-lg hover:cursor-pointer hover:bg-gray-200">
+                <img src="../assets/logout.svg" class="h-5"></img>
                 </button>
             </nav>
+            
     </div>
   </div>
 </template>
