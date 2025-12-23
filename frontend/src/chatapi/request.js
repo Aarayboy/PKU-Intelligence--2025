aac57913dacb68521f213b01441a3d13419aa396
YSZ <2300012984@stu.pku.edu.cn>
2025-12-23 22:35:19 +0800

    修了bug

--- frontend/src/chatapi/request.js
+++ frontend/src/chatapi/request.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: "http://127.0.0.1:8000",
-  timeout: 8000
-=======
-  baseURL: "http://127.0.0.1:8000"
->>>>>>> 73951398caa1c7d19c3b38d852910cf4d3d33660
+  timeout: 10000,
 });
 
 export default api;
